db999e9ba173738d91607c022e80c7298e4589b0
SoonHyunAn <ash7106@gmail.com>
2024-05-03 17:29:18 +0900

    Final_FlowNary_Frontend_2024_05_03

---
+++ src/main/java/com/example/flownary/entity/Reply.java
@@ -0,0 +1,42 @@
+package com.example.flownary.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Reply {
+
+	int rid;
+	int bid;
+	int uid;
+	String rContents;
+	LocalDateTime modTime;
+	int likeCount;
+	String nickname;
+	int isDeleted;
+	
+	public Reply(int bid, int uid, String rContents, String nickname) {
+		this.bid = bid;
+		this.uid = uid;
+		this.rContents = rContents;
+		this.nickname = nickname;
+	}
+
+	@Override
+	public String toString() {
+		return "Reply [rid=" + rid + ", bid=" + bid + ", uid=" + uid + ", rContents=" + rContents + ", modTime="
+				+ modTime + ", isDeleted=" + isDeleted + ", nickname=" + nickname + "]";
+	}
+
+
+
+}
